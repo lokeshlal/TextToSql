f5d80a73f455452ddc3c851e499d501fd0613ef6
Lokesh Lal <lokeshlal@gmail.com>
2020-04-20 08:38:42 +0530

    fixed issues

--- models/matcher.py
+++ models/matcher.py
@@ -1,19 +1,19 @@
 import copy
 
+
 class Matcher(object):
     def __init__(self):
         self.matcher = []
         return super().__init__()
-    
+
     def add(self, key, value):
         self.matcher.append((key, value))
-    
+
     def find(self, phrase):
         matches = []
         for match in self.matcher:
             if " " + str(match[1]) + " " in phrase \
-                or phrase.startswith(str(match[1]) + " ") \
-                or phrase.endswith(" " + str(match[1])):
+                    or phrase.startswith(str(match[1]) + " ") \
+                    or phrase.endswith(" " + str(match[1])):
                 matches.append(copy.copy(match))
         return matches
-    
